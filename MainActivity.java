14c95570498f1f1a43f47ca2034bb472eb15d2a7
zaki al faridzi <zalfaridzi0@gmail.com>
2020-09-28 14:36:02 +0700

    praktikum kedua android zaki al faridzi

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.kalkulator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
